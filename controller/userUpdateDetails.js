1b5cf5223c0ebe69711e27c0b344f861c798f53a
Mahammad Irshad <irshadalike10@gmail.com>
2024-07-14 15:28:15 +0530

    updaiton

--- controller/userUpdateDetails.js
+++ controller/userUpdateDetails.js
@@ -33,3 +33,10 @@ async function updateUserDetails(req,res){
 }
 
 module.exports=updateUserDetails
+
+// } catch (error) {
+//     return res.status(500).json({
+//         message:error.message||error,
+//         error:true
+//     })
+// }
